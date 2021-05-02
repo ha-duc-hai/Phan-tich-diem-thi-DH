f94d5622213e35b04efe6cf6518595e25f75defb
KhanhDuy <duydamkhac@gmail.com>
2021-05-02 23:17:03 +0700

    Update code

--- User Interface/display.py
+++ User Interface/display.py
@@ -11,7 +11,27 @@ render=ImageTk.PhotoImage(load)
 img=Label(root,image=render)
 img.place(x=0,y=0)
 
-name=Label(root,text="Tra số báo danh", fg="#FFFFFF")
+name=Label(root,text="Tra số báo danh", fg="#FFFFFF",bd=0,bg="#03152D")
+name.config(font=("Transformers Movie",30))
+name.pack(pady=1)
+
+box=Text(root,width=28,height=8,font=("ROBOTO",16))
+box.pack(pady=20)
+
+button_frame=Frame(root).pack(side=BOTTOM)
+
+def clear():
+    box.delete(1.0,END)
+    box1.delete(1.0,END)
+#def ......()
+
+#...._button=Button(button_frame,text="Tìm Số Báo Danh",font=(("Arial"),10,'bold'),bg='#303030',fg="#FFFFFF",command=....)
+#....
+clear_button=Button(button_frame,text="xóa số vừa nhập",font=(("Arial"),10,'bold'),bg='#303030',fg="#FFFFFF",command=clear)
+clear_button.place(x=150,y=310)
+box1=Text(root,width=28,height=8,font=("ROBOTO",16))
+box1.pack(pady=50)
+
 
 
 root.mainloop()
