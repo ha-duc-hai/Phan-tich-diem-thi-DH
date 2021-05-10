d70867181dd5a972cd2f65a68d0381fa3571a6b7
Hai <tankinguyen2@gmail.com>
2021-05-11 03:16:30 +0700

    week_5

--- get_dataSelenium.py
+++ get_dataSelenium.py
@@ -1,14 +1,23 @@
 from selenium import webdriver
 from time import sleep
 import pandas as pd
+from pandas import read_csv
 from selenium.webdriver.common.keys import Keys
-import csv
+
 # 1.khai bao bien brower
 brower = webdriver.Chrome(executable_path="./chromedriver.exe")# window 10
 # brower = webdriver.Chrome(executable_path="./chromedriver")# linux
 # 2.mo mot trang web
 brower.get("https://diemthi.vnexpress.net/#area=2&college=undefined&q=score1")
-for i in range(1, 5):# lấy 5 tỉnh
+
+data_sbd = []
+data_tinh = []
+data_toan = []
+data_van = []
+data_anh = []
+data_su = []
+data_dia = []
+for i in range(1, 20):# lấy 5 tỉnh
 	# Đợi trang web mở
 	sleep(5)
 
@@ -23,22 +32,37 @@ for i in range(1, 5):# lấy 5 tỉnh
 	local.send_keys(Keys.ENTER)
 	sleep(5)
 	# get data
-	data = brower.find_element_by_xpath('//*[@id="result"]/div[2]/div[2]/div/div[2]/div[3]/div/table/tbody')
+	data_ = brower.find_element_by_xpath('//*[@id="result"]/div[2]/div[2]/div/div[2]/div[3]/div/table/tbody')
 
-	# xuất data (Châu) -> csv
-	data = data.text
-	# data = data.split(' ')
-	print(data)
-	#data1 = open("data1.text","w+")
-	#data1.writelines(data)
-	#Xuli  = data1.readlines(data)
-	#diemtoan = []
-	#diemly = []
-	#with open('data.csv', mode='w') as file:
-	    #Xuli = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	    #Xuli.writerow(diemtoan)
-	    #Xuli.writerow(diemly)
-	# data1.close()
+	# xuất data -> csv
+	
+
+	data_ = data_.text
+	print(data_)
+	data_ = data_.split('\n')
+
+	for data in data_:
+		data = data.split(' ')
+		while len(data)<13:
+    			data.append('0')
+		data_sbd.append(data[1])
+		data_tinh.append(' '.join(data[2:6]))
+		data_toan.append(data[6])
+		data_van.append(data[7])
+		data_anh.append(data[8])
+		data_su.append(data[9])
+		data_dia.append(data[10])
+	
+	hoc_sinh = {'sbd':data_sbd,
+            	'tinh':data_tinh,
+          		'toan':data_toan,
+           		'van':data_van,
+           		'anh':data_anh,
+          		'su':data_su,
+          		'dia':data_dia}
+
+df = pd.DataFrame(hoc_sinh, columns = ['sbd', 'tinh','toan','van','anh','su','dia'])
+df.to_csv(r'data.csv')
 
 # 4.close brower
 brower.close()
---
+++ reports/week5.md
@@ -0,0 +1,18 @@
+# Báo cáo nhóm
+
+1. Done
+- Xuất data thu crawl được thành dạng csv, excel
+
+2. Issues
+- Kết nối data draw về với tkinder
+- data thu về chưa như mong muốn, cần xử lý thêm
+3. Plan next week
+- Phân tích
+- Phân công Thịnh, K.Duy tìm cách kết nối data với tkinder
+
+# Đóng góp của thành viên
+
+## Hà Đức Hải
+- Viết báo cáo tuần.
+- Xuất data -> csv
+
